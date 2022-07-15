ab44edc583824800dd2e70db5bd74e6fd6d09f5b
tjmuscles <tjmuscles@yahoo.com>
2022-07-14 22:18:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-6d-filter-ttajfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-6d-filter-ttajfx)
---
+++ index.ts
@@ -0,0 +1,27 @@
+import { Observable } from "rxjs";
+import { filter } from "rxjs/dist/types/operators";
+
+
+interface NewsItem {
+  category: 'Business' | 'Sports';
+  content: string;
+}
+
+const newsFeed$ = new Observable<NewsItem>(subscriber => {
+  setTimeout(() => {
+    subscriber.next({ category: 'Business', content: 'A' }), 1000});
+  setTimeout(() => {
+    subscriber.next({ category: 'Sports', content: 'B' }), 3000});
+  setTimeout(() => {
+    subscriber.next({ category: 'Business', content: 'C' }), 4000});
+  setTimeout(() => {
+    subscriber.next({ category: 'Sports', content: 'D' }), 6000});
+  setTimeout(() => {
+    subscriber.next({ category: 'Business', content: 'E' }), 7000});
+});
+
+newsFeed$.pipe(
+  filter(item => item.category === 'Sports')
+).subscribe(
+  item => console.log(item)
+);
